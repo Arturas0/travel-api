10e9278c5d8947a55f07a2143bd605cd5952809b
Arturas <arturas.bnr@gmail.com>
2023-06-16 22:48:21 +0300

    changed controller location & updated Readme

--- app/Http/Controllers/Api/v1/TravelController.php
+++ app/Http/Controllers/Api/v1/TravelController.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\v1;
 
-use App\Http\Resources\TravelResource;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\v1\TravelResource;
 use App\Models\Travel;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
--- routes/v1/travels.php
+++ routes/v1/travels.php
@@ -2,8 +2,7 @@
 
 declare(strict_types=1);
 
-
-use App\Http\Controllers\TravelController;
+use App\Http\Controllers\Api\v1\TravelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TravelController::class, 'index'])->name('index');
