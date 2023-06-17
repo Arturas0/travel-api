25de754d5bb52f5da2de9ea975634efa761c0046
Arturas <arturas.bnr@gmail.com>
2023-06-17 21:29:18 +0300

    added login route with controller

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
