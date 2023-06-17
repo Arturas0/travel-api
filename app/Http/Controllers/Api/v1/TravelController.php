d7f8622c1b8ee29ba30fc47d475b451513c16652
Arturas <arturas.bnr@gmail.com>
2023-06-17 15:32:14 +0300

    after review: moved tours() method to TourController, added custom validation message and error test

--- app/Http/Controllers/Api/v1/TravelController.php
+++ app/Http/Controllers/Api/v1/TravelController.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\TourRequest;
-use App\Http\Resources\v1\TourResource;
 use App\Http\Resources\v1\TravelResource;
 use App\Models\Travel;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
@@ -18,20 +16,6 @@ class TravelController extends Controller
         return TravelResource::collection(
             Travel::query()
                 ->where('is_public', '=', true)
-                ->paginate()
-        );
-    }
-
-    public function tours(Travel $travel, TourRequest $request): AnonymousResourceCollection
-    {
-        return TourResource::collection(
-            $travel->tours()
-                ->searchByPriceFrom($request->input('priceFrom'))
-                ->searchByPriceTo($request->input('priceTo'))
-                ->searchByDateFrom($request->input('dateFrom'))
-                ->searchByDateTo($request->input('dateTo'))
-                ->sortByPrice($request->input('sortByPrice'))
-                ->sortByStartDateAsc()
                 ->paginate()
         );
     }
--- routes/v1/travels.php
+++ routes/v1/travels.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\Api\v1\TourController;
 use App\Http\Controllers\Api\v1\TravelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TravelController::class, 'index'])->name('index');
-Route::get('{travel}/tours', [TravelController::class, 'tours'])->name('tours');
+Route::get('{travel}/tours', [TourController::class, 'index'])->name('tours');
