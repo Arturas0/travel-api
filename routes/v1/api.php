4dfcff1a41784360dfd75316190fb3b1ce0c03e3
Arturas <arturas.bnr@gmail.com>
2023-06-18 10:43:00 +0300

    fixed login method, added tests for login route

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
+include __DIR__.'/auth.php';
+
 Route::prefix('travels')->as('travels.')->group(
     base_path('routes/v1/travels.php'),
-);
-
-Route::prefix('auth')->as('auth.')->group(
-    base_path('routes/v1/auth.php'),
 );
