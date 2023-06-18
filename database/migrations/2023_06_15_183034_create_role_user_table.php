a1b800e5fed9f0834936573f56537db4e87a5728
Arturas <arturas.bnr@gmail.com>
2023-06-18 21:31:08 +0300

    updates

--- database/migrations/2023_06_15_183034_create_role_user_table.php
+++ database/migrations/2023_06_15_183034_create_role_user_table.php
@@ -19,9 +19,4 @@ return new class extends Migration
             $table->unique(['user_id', 'role_id']);
         });
     }
-
-    public function down(): void
-    {
-        Schema::dropIfExists('role_user');
-    }
 };
